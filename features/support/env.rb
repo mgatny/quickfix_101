69d47598b993bd023ca86ebb5ed78c384f262ca5
Mike Gatny <mgatny@connamara.com>
2013-11-14 05:34:46 -0600

    updated readme

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,16 +5,29 @@ require 'rspec'
 require 'anticipate'
 require 'spoon'
 
-World(Anticipate)
+#---- Start the app in the background, and stash the pid so we can kill it later:
+
+$app_pid = nil
+def start_app symbol
+    $app_pid = Spoon.spawnp("./app", "etc/app.cfg", symbol)
+end
+
+#---- At exit, kill the app if it is still running:
+
+at_exit do
+    AgentFIX.stop
+    unless $app_pid.nil?
+        puts "Killing app (pid=#{$app_pid})"
+        Process.kill("TERM", $app_pid)
+        Process.waitall
+    end
+end
+
+#---- Set up fix_spec and agent_fix:
 
 FIXSpec::data_dictionary = FIXSpec::DataDictionary.new "etc/FIX42.xml"
 AgentFIX.include_session_level = true
 AgentFIX.start
-@app_pid = Spoon.spawnp("./app", "etc/app.cfg", "AAPL") #symbol)
 
-at_exit do
-    AgentFIX.stop
-    Process.kill("TERM", @app_pid) unless @app_pid.nil?
-    Process.waitall
-end
+World(Anticipate)
 
